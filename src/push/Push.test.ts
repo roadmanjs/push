372e4d08216101a1196844241a9c7936c3f98ce3
ceddybi <ceddymuhoza@gmail.com>
2022-11-25 10:43:07 -0500

    sendMessageToTopic

--- src/push/Push.test.ts
+++ src/push/Push.test.ts
@@ -1,30 +1,53 @@
 import 'reflect-metadata';
 import 'mocha';
 
+import {sendMessageToTopic, sendMessageToUser} from './push.methods';
+
 import {expect} from 'chai';
 import {startCouchbase} from '@roadmanjs/couchset';
-import {sendMessageToUser} from './push.methods';
 
-before((done) => {
-    startCouchbase().then(() => done());
-});
+// before((done) => {
+//     startCouchbase().then(() => done());
+// });
 
 describe('Push', () => {
+    // it('it should send a notification to user', async () => {
+    //     const userId = '99bc43ba-02ab-4394-b48b-49a39a95443c';
+    //     const sentMessage = await sendMessageToUser(userId, {
+    //         data: {
+    //             type: 'tv',
+    //         },
+    //         notification: {
+    //             title: 'Basic Notification',
+    //             body: 'This is a basic notification sent from the server!',
+    //             imageUrl: 'https://my-cdn.com/app-logo.png',
+    //             sound: 'vuga_zing',
+    //         },
+    //     });
+
+    //     console.log("sendMessageToUser", sentMessage)
+
+    //     expect(sentMessage).to.be.not.empty;
+    // });
+
     it('it should send a notification to user', async () => {
-        const userId = '99bc43ba-02ab-4394-b48b-49a39a95443c';
-        const sentMessage = await sendMessageToUser(userId, {
-            data: {
-                type: 'tv',
-            },
-            notification: {
-                title: 'Basic Notification',
-                body: 'This is a basic notification sent from the server!',
-                imageUrl: 'https://my-cdn.com/app-logo.png',
-                sound: 'vuga_zing',
+        const topic = 'Tv';
+        const sentMessage = await sendMessageToTopic({
+            topic,
+            payload: {
+                data: {
+                    type: 'tv',
+                },
+                notification: {
+                    title: 'Notification Test',
+                    body: 'Notification Test',
+                    // imageUrl: 'https://my-cdn.com/app-logo.png',
+                    sound: 'vuga_zing',
+                },
             },
         });
 
-        console.log("sendMessageToUser", sentMessage)
+        console.log('sendMessageToTopic', sentMessage);
 
         expect(sentMessage).to.be.not.empty;
     });
